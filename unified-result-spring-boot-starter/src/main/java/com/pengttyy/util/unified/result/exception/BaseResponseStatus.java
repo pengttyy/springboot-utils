fa35c52c13abe02bb8a64350d8ef211e7e421bb5
object <pengttyy@gmail.com>
2021-07-24 12:44:27 +0800

    Update BaseResponseStatus.java

--- unified-result-spring-boot-starter/src/main/java/com/pengttyy/util/unified/result/exception/BaseResponseStatus.java
+++ unified-result-spring-boot-starter/src/main/java/com/pengttyy/util/unified/result/exception/BaseResponseStatus.java
@@ -1,7 +1,4 @@
-/*
- * frxs Inc.  湖南兴盛优选电子商务有限公司.
- * Copyright (c) 2017-2021. All Rights Reserved.
- */
+
 package com.pengttyy.util.unified.result.exception;
 
 import org.springframework.http.HttpStatus;
